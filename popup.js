93d22c55f630e3d70565787a2fb8748393de30c7
yolles <yolles@adobe.com>
2014-10-26 15:29:23 -0700

    remove options page inline options style accordion etc...

--- popup.js
+++ popup.js
@@ -1,4 +1,7 @@
-var app = angular.module('PopupApp', ['ngStorage']);
+var app = angular.module('PopupApp', ['ngStorage', 'ui.bootstrap']),
+    MANIFEST_URL = 'https://raw.githubusercontent.com/nateyolles/aem-developer-chrome/master/manifest.json',
+    cachedEventPage,
+    pageDetails;
 
 app.controller('PopupController', function($scope, $localStorage, $http){
 
@@ -8,6 +11,27 @@ app.controller('PopupController', function($scope, $localStorage, $http){
       {name: 'Local Publish', url: 'http://localhost:4503'}
     ]
   });
+
+  $scope.version = {
+    current: chrome.app.getDetails().version,
+    latest: 0
+  };
+
+  $scope.editMode = false;
+
+  $scope.changeEditMode = function() {
+    $scope.editMode = !$scope.editMode;
+  }
+
+  $scope.remove = function(index) {
+    $scope.options.servers.splice(index, 1);
+  };
+
+  $scope.add = function() {
+    $scope.options.servers.push({name: $scope.newServer.name, url: $scope.newServer.url});
+    $scope.newServer.name = '';
+    $scope.newServer.url = '';
+  };
 
   $scope.redirectToEnvironment = function(index){
     var newOrigin = $scope.options.servers[index].url;
@@ -20,188 +44,188 @@ app.controller('PopupController', function($scope, $localStorage, $http){
     setTabLocation(newOrigin + pageDetails.location.pathname + pageDetails.location.search + pageDetails.location.hash);
   };
 
-  $scope.version = {
-    current: chrome.app.getDetails().version,
-    latest: 0
-  };
-
-
-  var responsePromise = $http.get('https://raw.githubusercontent.com/nyolles/aem-developer-chrome/master/manifest.json');
+  var responsePromise = $http.get(MANIFEST_URL);
 
   responsePromise.success(function(data, status, headers, config) {
       $scope.version.latest = data.version;
   });
 });
 
-// This callback function is called when the content script has been 
-// injected and returned its results
-var cachedEventPage,
-    pageDetails;
-
-// When the popup HTML has loaded
 window.addEventListener('load', function(evt) {
-    // Get the event page
-    chrome.runtime.getBackgroundPage(function(eventPage) {
-        // Call the getPageInfo function in the event page, passing in 
-        // our onPageDetailsReceived function as the callback. This injects 
-        // content.js into the current tab's HTML
-        eventPage.getPageDetails(function(tab){
-          tab.location = normalizeLocation(tab.location);
-          pageDetails = tab;
-        });
-        cachedEventPage = eventPage;
+  chrome.runtime.getBackgroundPage(function(eventPage) {
+    // Call the getPageInfo function in the event page, passing in 
+    // our onPageDetailsReceived function as the callback. This injects 
+    // content.js into the current tab's HTML
+    eventPage.getPageDetails(function(tab){
+      tab.location = normalizeLocation(tab.location);
+      pageDetails = tab;
     });
+    cachedEventPage = eventPage;
+  });
 
-    $('a.redirect').click(function(e){
-      e.preventDefault();
-      setTabLocation(pageDetails.location.origin + $(this).attr('data-link'));
-      window.close();
-    });
+  $('button').click(function(e){
+    var $this = $(this),
+        $btnGroup = $this.parents('.btn-group');
 
-    $('#lnk_clearClientLibs').click(function(e){
-      e.preventDefault();
-      cachedEventPage.executeScript('AemDeveloper.clearClientLibs()', function(a){ $('#status').text(JSON.stringify(a));});
-      //window.close();
-    });
+    $btnGroup.find('button').removeClass('active');
+    $(this).addClass('active');
+  });
 
-    $('#lnk_clearCompiledJSPs').click(function(e){
-      e.preventDefault();
-      cachedEventPage.executeScript('AemDeveloper.clearCompiledJSPs()', function(a){ $('#status').text(JSON.stringify(a));});
-      //window.close();
-    });
+  $('a.redirect').click(function(e){
+    e.preventDefault();
+    setTabLocation(pageDetails.location.origin + $(this).attr('data-link'));
+    window.close();
+  });
 
-    $('#lnk_digitalPulseDebugger').click(function(e){
-      e.preventDefault();
-      cachedEventPage.executeScript('AemDeveloper.openDigitalPulseDebugger()', function(a){ $('#status').text(JSON.stringify(a));});
-      //window.close();
-    });
+  $('#lnk_clearClientLibs').click(function(e){
+    e.preventDefault();
+    cachedEventPage.executeScript('AemDeveloper.clearClientLibs()', function(a){ $('#status').text(JSON.stringify(a));});
+    //window.close();
+  });
 
-    $('#lnk_clientContextWindow').click(function(e){
-      e.preventDefault();
-      cachedEventPage.executeScript('AemDeveloper.openClientContextWindow()', function(a){ $('#status').text(JSON.stringify(a));});
-    });
+  $('#lnk_clearCompiledJSPs').click(function(e){
+    e.preventDefault();
+    cachedEventPage.executeScript('AemDeveloper.clearCompiledJSPs()', function(a){ $('#status').text(JSON.stringify(a));});
+    //window.close();
+  });
 
-    $('#lnk_siteAdminToggle').click(function(e){
-      e.preventDefault();
+  $('#lnk_digitalPulseDebugger').click(function(e){
+    e.preventDefault();
+    cachedEventPage.executeScript('AemDeveloper.openDigitalPulseDebugger()', function(a){ $('#status').text(JSON.stringify(a));});
+    //window.close();
+  });
 
-      if (pageDetails.location.href.indexOf('/cf#/') !== -1) {
-        setTabLocation(pageDetails.location.href.split('/cf#').join(''));
-      } else {
-        setTabLocation(pageDetails.location.origin + '/cf#' + pageDetails.location.pathname + pageDetails.location.search + pageDetails.location.hash);
-      }
+  $('#lnk_clientContextWindow').click(function(e){
+    e.preventDefault();
+    cachedEventPage.executeScript('AemDeveloper.openClientContextWindow()', function(a){ $('#status').text(JSON.stringify(a));});
+  });
 
-      window.close();
-    });
+  $('#lnk_siteAdminToggle').click(function(e){
+    e.preventDefault();
 
-    $('.querystring').click(function(e){
-      e.preventDefault();
-
-      var $this = $(this),
-          key = $this.attr('data-querystring-key'),
-          value = $this.attr('data-querystring-value') || null;
-
-      setTabLocation(getUrlWithUpdatedQueryString(pageDetails.location, key, value));
-
-      window.close();
-    });
-
-    /**
-     * Create a psudo location object fixing browser problems when the Location object
-     * contains '/cf#/'.
-     *
-     * @param {location} location - the location object to read from.
-     * @returns {Object} A psudo location object.
-     */
-    function normalizeLocation(location) {
-      var origin = location.origin,
-          search,
-          pathname,
-          hash,
-          tempLocation;
-
-      if (location.href.indexOf('/cf#/') === -1) {
-        pathname = location.pathname;
-        hash = location.hash;
-        search = location.search;
-      } else {
-        tempLocation = document.createElement('a');
-        tempLocation.href = location.href.split('/cf#').join('');
-
-        pathname = '/cf#' + tempLocation.pathname;
-        hash = tempLocation.hash;
-        search = tempLocation.search;
-      }
-
-      return {
-        href : origin + pathname + search + hash,
-        origin : origin,
-        search : search,
-        pathname : pathname,
-        hash : hash,
-        port: location.port,
-        host: location.host,
-        hostname: location.hostname,
-        protocol: location.protocol
-      }
+    if (pageDetails.location.href.indexOf('/cf#/') !== -1) {
+      setTabLocation(pageDetails.location.href.split('/cf#').join(''));
+    } else {
+      setTabLocation(pageDetails.location.origin + '/cf#' + pageDetails.location.pathname + pageDetails.location.search + pageDetails.location.hash);
     }
 
-    /**
-     * Update the query string of a URL while maintaining other params and the hash.
-     *
-     * If value is null the parameter is removed from the query string.
-     * If value is empty string (''), the parameter is added to the query string
-     * with no value.
-     *
-     * @param {location} location - The location
-     * @param {string} key - The query string parameter to update.
-     * @param {string} value - the query string parameter's value to update.
-     * @returns {string} New URL with updated query string parameters.
-     */
-    function getUrlWithUpdatedQueryString(location, key, value) {
-      var origin = location.origin,
-          hash = location.hash,                 // starts with #
-          search = location.search,             // starts with ?
-          pathname = location.pathname,         // starts with /
-          queryParams = {},
-          updatedSearchString,
-          isFirstParam = true;
+    window.close();
+  });
 
-      if (search) {
-        queryParams = getQueryParameters(search);     // returns object
-      }
+  $('.querystring').click(function(e){
+    e.preventDefault();
 
-      if (value !== null) {
-        queryParams[key] = value;               // overwrite or add key/value
-      } else {
-        delete queryParams[key];
-      }
+    var $this = $(this),
+        key = $this.attr('data-querystring-key'),
+        value = $this.attr('data-querystring-value') || null;
 
-      if (Object.keys(queryParams).length === 0) {
-        updatedSearchString = '';
-      } else {
-        // loop through object and create string
-        for (var prop in queryParams) {
-          if (queryParams.hasOwnProperty(prop)) {
-            if (isFirstParam) {
-              updatedSearchString = '?';
-              isFirstParam = false;
-            } else {
-              updatedSearchString += '&';
-            }
+    setTabLocation(getUrlWithUpdatedQueryString(pageDetails.location, key, value));
 
-            updatedSearchString += prop;
-
-            if (queryParams[prop]) {
-              updatedSearchString += '=' + queryParams[prop];
-            }
-          }
-        }
-      }
-
-      return origin + pathname + updatedSearchString + hash;
-    }    
+    window.close();
+  });   
 });
 
+/**
+ * Create a psudo location object fixing browser problems when the Location object
+ * contains '/cf#/'.
+ *
+ * @param {location} location - the location object to read from.
+ * @returns {Object} A psudo location object.
+ */
+function normalizeLocation(location) {
+  var origin = location.origin,
+      search,
+      pathname,
+      hash,
+      tempLocation;
+
+  if (location.href.indexOf('/cf#/') === -1) {
+    pathname = location.pathname;
+    hash = location.hash;
+    search = location.search;
+  } else {
+    tempLocation = document.createElement('a');
+    tempLocation.href = location.href.split('/cf#').join('');
+
+    pathname = '/cf#' + tempLocation.pathname;
+    hash = tempLocation.hash;
+    search = tempLocation.search;
+  }
+
+  return {
+    href : origin + pathname + search + hash,
+    origin : origin,
+    search : search,
+    pathname : pathname,
+    hash : hash,
+    port: location.port,
+    host: location.host,
+    hostname: location.hostname,
+    protocol: location.protocol
+  }
+}
+
+/**
+ * Update the query string of a URL while maintaining other params and the hash.
+ *
+ * If value is null the parameter is removed from the query string.
+ * If value is empty string (''), the parameter is added to the query string
+ * with no value.
+ *
+ * @param {location} location - The location
+ * @param {string} key - The query string parameter to update.
+ * @param {string} value - the query string parameter's value to update.
+ * @returns {string} New URL with updated query string parameters.
+ */
+function getUrlWithUpdatedQueryString(location, key, value) {
+  var origin = location.origin,
+      hash = location.hash,                 // starts with #
+      search = location.search,             // starts with ?
+      pathname = location.pathname,         // starts with /
+      queryParams = {},
+      updatedSearchString,
+      isFirstParam = true;
+
+  if (search) {
+    queryParams = getQueryParameters(search);     // returns object
+  }
+
+  if (value !== null) {
+    queryParams[key] = value;               // overwrite or add key/value
+  } else {
+    delete queryParams[key];
+  }
+
+  if (Object.keys(queryParams).length === 0) {
+    updatedSearchString = '';
+  } else {
+    // loop through object and create string
+    for (var prop in queryParams) {
+      if (queryParams.hasOwnProperty(prop)) {
+        if (isFirstParam) {
+          updatedSearchString = '?';
+          isFirstParam = false;
+        } else {
+          updatedSearchString += '&';
+        }
+
+        updatedSearchString += prop;
+
+        if (queryParams[prop]) {
+          updatedSearchString += '=' + queryParams[prop];
+        }
+      }
+    }
+  }
+
+  return origin + pathname + updatedSearchString + hash;
+}
+
+/**
+ * Update the browser's location
+ *
+ * @param {string} url - The location the browser should navigate to.
+ */
 function setTabLocation(url) {
   chrome.tabs.update(null, {url: url});
 }
