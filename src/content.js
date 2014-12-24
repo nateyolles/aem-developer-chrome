79f922b6e69fdf14a46682b832420e90c90d2673
yolles <yolles@adobe.com>
2014-12-24 12:52:03 -0800

    Digital Pulse Debugger - focus window on multiple clicks

--- src/content.js
+++ src/content.js
@@ -32,7 +32,10 @@ var AemDeveloper = (function(window, undefined) {
    * Open the marketing cloud debugger window.
    */
   function openDigitalPulseDebugger() {
-    window.open("","dp_debugger","width=700,height=1000,location=0,menubar=0,status=1,toolbar=0,resizable=1,scrollbars=1").document.write("<script language=\"JavaScript\" id=dbg src=\"https://www.adobetag.com/d1/digitalpulsedebugger/live/DPD.js\"></"+"script>");
+    var dp_debugger = window.open("","dp_debugger","width=700,height=1000,location=0,menubar=0,status=1,toolbar=0,resizable=1,scrollbars=1");
+    dp_debugger.document.write("<script language=\"JavaScript\" id=dbg src=\"https://www.adobetag.com/d1/digitalpulsedebugger/live/DPD.js\"></"+"script>");
+    dp_debugger.focus();
+
     chrome.runtime.sendMessage({
       type: 'dp_debugger',
       status: 'success'
