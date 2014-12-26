7300ee612517e1605d354718f8b3db37217c42e0
yolles <yolles@adobe.com>
2014-12-26 08:56:15 -0800

    Compare
     - Add Shadow DOM
     - cleanup code
     - comment
     - break out into functions

--- src/content.js
+++ src/content.js
@@ -18,15 +18,26 @@ var AemDeveloper = (function(window, undefined) {
    * @private
    * @global
    */
-  var CLIENTLIB_QUERY =    '/crx/de/query.jsp?type=xpath&stmt=/jcr:root/var/clientlibs/*&showResults=true',
-      COMPILED_JSP_QUERY = '/crx/de/query.jsp?type=xpath&stmt=/jcr:root/var/classes//jsp&showResults=true',
-      USER_INFO =          '/libs/granite/security/currentuser.json'
-      PRODUCT_INFO =       '/libs/cq/core/productinfo.json',
-      SLING_INFO =         '/system/console/status-slingsettings.json',
-      SYSTEM_INFO =        '/system/console/status-System%20Properties.json',
-      SUDOABLE_INFO =      '.sudoables.json',
-      MEMORY_USAGE =       '/system/console/memoryusage',
-      GARBAGE_COLLECTOR =  'command=gc';
+  var CLIENTLIB_QUERY         = '/crx/de/query.jsp?type=xpath&stmt=/jcr:root/var/clientlibs/*&showResults=true',
+      COMPILED_JSP_QUERY      = '/crx/de/query.jsp?type=xpath&stmt=/jcr:root/var/classes//jsp&showResults=true',
+      USER_INFO               = '/libs/granite/security/currentuser.json'
+      PRODUCT_INFO            = '/libs/cq/core/productinfo.json',
+      SLING_INFO              = '/system/console/status-slingsettings.json',
+      SYSTEM_INFO             = '/system/console/status-System%20Properties.json',
+      SUDOABLE_INFO           = '.sudoables.json',
+      MEMORY_USAGE            = '/system/console/memoryusage',
+      GARBAGE_COLLECTOR       = 'command=gc',
+      COMPARE_CONTAINER_NAME  = 'aem-developer-chrome-diff',
+      TITLE_BAR_CLASS_NAME    = 'titlebar',
+      COMPARE_BAR_CLASS_NAME  = 'comparebar',
+      CONTAINER_CLASS_NAME    = 'container',
+      COMPARE_TEXT            = 'Compare',
+      TITLE_CLASS_NAME        = 'title',
+      TOGGLE_CLASS_NAME       = 'toggle',
+      TOGGLE_HIDE_ALL_TEXT    = 'Show Difference',
+      TOGGLE_SHOW_ALL_TEXT    = 'Show All',
+      CLOSE_CLASS_NAME        = 'close',
+      CLOSE_TITLE_TEXT        = 'Close compare modal';
 
   /**
    * Open the marketing cloud debugger window.
@@ -158,6 +169,11 @@ var AemDeveloper = (function(window, undefined) {
     xmlhttp.send();
   }
 
+  /**
+   * Get list of users that can be impersonated.
+   *
+   * @param {String} Path to user e.g. '/home/users/a/admin'
+   */
   function getSudoables(home) {
     getInfo('sudoables', home + SUDOABLE_INFO);
   }
@@ -176,6 +192,7 @@ var AemDeveloper = (function(window, undefined) {
    */
   function getUserInfo() {
     getInfo('user', USER_INFO, function(data){
+      console.dir(data.home);
       getSudoables(data.home);
     });
   }
@@ -227,6 +244,151 @@ var AemDeveloper = (function(window, undefined) {
     xmlhttp.send(GARBAGE_COLLECTOR);
   }
 
+  /**
+   * Escape HTML String. Not to be used for XSS protection.
+   *
+   * @param {String} String to escape.
+   * @returns {String} Escaped string.
+   */
+  function escapeHTML(str) {
+    var div = document.createElement('div'),
+        text = document.createTextNode(str);
+
+    div.appendChild(text);
+    return div.innerHTML;
+  }
+
+  /**
+   * Remove provided keys from JSON objects.
+   *
+   * @param {JSON} JSON object.
+   * @param {String[]} Array of Strings to remove from JSON object.
+   */
+  function scrubJson(obj, propertiesToRemove) {
+    for (key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        if (propertiesToRemove.indexOf(key) !== -1) {
+          delete obj[key];
+        } else if (typeof obj[key] === 'object') {
+          scrubJson(obj[key], propertiesToRemove);
+        } else {
+          obj[key] = escapeHTML(obj[key]);
+        }
+      }
+    }
+  }
+
+  /**
+   * Get the difference between two JSON objects in jsondiffpatch HTML format.
+   *
+   * @param {JSON} JSON object to be compared against
+   * @param {JSON} JSON object to be compared with
+   * @returns {String} The delta of the two JSON objects formatted in HTML.
+   * @see {@link https://github.com/benjamine/jsondiffpatch/blob/master/docs/formatters.md} for further information.
+   * @requires {@link https://github.com/benjamine/jsondiffpatch}
+   */
+  function getDifferenceHtml(currentJson, compareJson) {
+    var instance = jsondiffpatch.create({
+      objectHash: function(obj) {
+        return obj.name;
+      }
+    });
+
+    var propertiesToRemove = ['jcr:createdBy', 'cq:lastModified', 'cq:lastModifiedBy',
+                              'jcr:created', 'jcr:lastModified', 'jcr:lastModifiedBy',
+                              'jcr:baseVersion', 'jcr:isCheckedOut', 'jcr:predecessors',
+                              'jcr:uuid', 'jcr:versionHistory', 'cq:lastReplicated'];
+
+    scrubJson(currentJson, propertiesToRemove);
+    scrubJson(compareJson, propertiesToRemove);
+
+    var delta = instance.diff(compareJson, currentJson);
+
+    return jsondiffpatch.formatters.html.format(delta, compareJson);
+  }
+
+  /**
+   * Create and add differential HTML to the page.
+   *
+   * @param {HTMLElement} The DOM node to attach the Shadow DOM and HTML difference to.
+   * @param {String} HTML difference to insert into page.
+   * @param {String} The current origin to compare against (e.g. 'http://localhost:4502').
+   * @param {String} The origin to compare with (e.g. 'http://localhost:4503').
+   * @param {String} The path of the page to compare (e.g '/content/project/en_us/about.html). 
+   * @see {@link https://github.com/benjamine/jsondiffpatch/blob/master/docs/formatters.md} for further information.
+   * @requires {@link https://github.com/benjamine/jsondiffpatch}
+   */
+  function createDifferenceHtml(insertionPoint, html, origin, compareToOrigin, path) {
+    var visualdiff = document.createElement('div'),
+        diffToggle = document.createElement('a'),
+        titleSpan = document.createElement('span'),
+        close = document.createElement('a'),
+        titleBar = document.createElement('div'),
+        compareBar = document.createElement('div'),
+        left = document.createElement('span'),
+        right = document.createElement('span'),
+        container = document.createElement('div'),
+        root = container.createShadowRoot();
+        isShowingAll = false;
+
+    titleSpan.innerHTML = COMPARE_TEXT;
+    titleSpan.className = TITLE_CLASS_NAME;
+
+    close.href = '#';
+    close.className = CLOSE_CLASS_NAME;
+    close.title = CLOSE_TITLE_TEXT;
+    close.onclick = function(evt){
+      container.parentElement.removeChild(container);
+      evt.preventDefault();
+    };
+
+    diffToggle.href = '#';
+    diffToggle.innerHTML = TOGGLE_SHOW_ALL_TEXT;
+    diffToggle.className = TOGGLE_CLASS_NAME;
+    diffToggle.onclick = function(evt){
+      if (isShowingAll) {
+        jsondiffpatch.formatters.html.hideUnchanged(visualdiff);
+        diffToggle.innerHTML = TOGGLE_SHOW_ALL_TEXT;
+      } else {
+        jsondiffpatch.formatters.html.showUnchanged(true, visualdiff);
+        diffToggle.innerHTML = TOGGLE_HIDE_ALL_TEXT;
+      }
+
+      isShowingAll = !isShowingAll;
+      evt.preventDefault();
+    };
+
+    titleBar.className = TITLE_BAR_CLASS_NAME;
+    titleBar.appendChild(titleSpan);
+    titleBar.appendChild(diffToggle);
+    titleBar.appendChild(close);
+
+    left.innerHTML = '<span class="origin">' + origin + '</span>' + path;
+    right.innerHTML = '<span class="origin">' + compareToOrigin + '</span>' + path;
+
+    compareBar.className = COMPARE_BAR_CLASS_NAME;
+    compareBar.appendChild(left);
+    compareBar.appendChild(right);
+
+    visualdiff.innerHTML = html;
+    visualdiff.className = CONTAINER_CLASS_NAME;
+    visualdiff.appendChild(titleBar);
+    visualdiff.appendChild(compareBar);
+
+    jsondiffpatch.formatters.html.hideUnchanged(visualdiff);
+
+    container.id = COMPARE_CONTAINER_NAME;
+
+    root.appendChild(visualdiff);
+
+    insertionPoint.appendChild(container);
+  }
+
+  /**
+   * Compare page to the same page on a given domain.
+   *
+   * @param {String} Domain to compare current page to (e.g. 'http://localhost:4503'). 
+   */
   function comparePage(compareToOrigin) {
     var path = location.pathname.replace('.html', '/jcr:content.-1.json');
 
@@ -242,127 +404,22 @@ var AemDeveloper = (function(window, undefined) {
 
         comparePageRequest.onreadystatechange = function(){
           if (comparePageRequest.readyState === 4 && comparePageRequest.status === 200) {
-            console.log('got both');
 
-            var instance = jsondiffpatch.create({
-                objectHash: function(obj) {
-                    return obj.name;
-                }
-            })
+            var body = document.querySelector('body'),
+                html = getDifferenceHtml(currentPageRequest.response, comparePageRequest.response),
+                oldContainer = document.getElementById(COMPARE_CONTAINER_NAME);
 
-            var currentJson = currentPageRequest.response;
-            var compareJson = comparePageRequest.response;
-
-            var propertiesToIgnore = ['jcr:createdBy', 'cq:lastModified', 'cq:lastModifiedBy',
-                                      'jcr:created', 'jcr:lastModified', 'jcr:lastModifiedBy',
-                                      'jcr:baseVersion', 'jcr:isCheckedOut', 'jcr:predecessors',
-                                      'jcr:uuid', 'jcr:versionHistory', 'cq:lastReplicated'];
-            function loopJson(obj) {
-              for (key in obj) {
-                if (obj.hasOwnProperty(key)) {
-                  if (propertiesToIgnore.indexOf(key) !== -1) {
-                    delete obj[key];
-                  } else if (typeof obj[key] === 'object') {
-                    loopJson(obj[key]);
-                  } else {
-                    obj[key] = escapeHTML(obj[key]);
-                  }
-                }
-              }
+            if (oldContainer) {
+              body.removeChild(oldContainer);
             }
 
-            loopJson(currentJson);
-            loopJson(compareJson);
-
-            var delta = instance.diff(compareJson, currentJson);
-            var visualdiff = document.createElement('div');
-            visualdiff.className = 'aem-developer-chrome-diff';
-            
-
-            function escapeHTML (str)
-            {
-                var div = document.createElement('div');
-                var text = document.createTextNode(str);
-                div.appendChild(text);
-                return div.innerHTML;
-            }
-
-            var diffToggle = document.createElement('a');
-            diffToggle.href = '#';
-            diffToggle.innerHTML = 'Show All';
-            diffToggle.className = "aem-developer-chrome-diff-toggle";
-            var flag = false;
-            diffToggle.onclick = function(evt){
-              if (flag) {
-                jsondiffpatch.formatters.html.hideUnchanged();
-                diffToggle.innerHTML = 'Show All';
-              }
-              else {
-                jsondiffpatch.formatters.html.showUnchanged();
-                diffToggle.innerHTML = 'Show Difference';
-              }
-
-              flag = !flag;
-              evt.preventDefault();
-            };
-
-            var titleSpan = document.createElement('span');
-            titleSpan.innerHTML = 'Compare';
-            titleSpan.className = 'aem-developer-chrome-diff-title';
-
-            var close = document.createElement('a');
-            close.href = '#';
-            close.className = 'aem-developer-chrome-diff-close';
-            close.title = 'Close compare modal'
-            close.onclick= function(evt){
-              visualdiff.parentElement.removeChild(visualdiff);
-              evt.preventDefault();
-            };
-
-
-            var titleBar = document.createElement('div');
-            titleBar.className = 'aem-developer-chrome-diff-titlebar';
-            titleBar.appendChild(titleSpan);
-            titleBar.appendChild(diffToggle);
-            titleBar.appendChild(close);
-
-
-
-
-
-            
-            visualdiff.innerHTML = jsondiffpatch.formatters.html.format(delta, compareJson);
-            jsondiffpatch.formatters.html.hideUnchanged();
-
-            
-
-            document.querySelectorAll('body')[0].appendChild(visualdiff);
-            visualdiff.appendChild(titleBar);
-            var scripts = visualdiff.querySelectorAll('script');
-            for (var i = 0; i < scripts.length; i++) {
-              eval(scripts[i].innerHTML);
-            }
-
-            // var a = document.getElementsByClassName('jsondiffpatch-child-node-type-object');
-
-            // for (var x = 0; x < a.length; x++) {
-            //   a[x].addEventListener('click', function(e){
-            //     var b = this.getElementsByClassName('jsondiffpatch-unchanged');
-            //     for (var y = 0; y < b.length; y++) {
-            //       if (b[y].style.maxHeight === 'inherit') {
-            //         b[y].style.maxHeight = '0';
-            //       } else {
-            //         b[y].style.maxHeight = 'inherit';
-            //       }
-            //     }
-            //   });
-            // }
+            createDifferenceHtml(body, html, location.origin, compareToOrigin, path);
           }
-        }
+        };
 
         comparePageRequest.send();
       }
-    }
+    };
 
     currentPageRequest.send();
   }
